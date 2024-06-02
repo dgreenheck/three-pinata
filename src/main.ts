d4ed9c095ca1e7c86c46c1bc638f36cd66f1df86
Daniel Greenheck <dgreenheck@gmail.com>
2024-06-01 22:49:44 -0500

    Temp fix for Rapier WASM issue

--- src/main.ts
+++
@@ -1,6 +0,0 @@
-import { init } from "./app";
-
-// Dynamically import the Rapier WASM module
-import("@dimforge/rapier3d").then((RAPIER) => {
-  init(RAPIER);
-});
--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,10 @@ export default {
   build: {
     outDir: './dist',
     sourcemap: true,
+    rollupOptions: {
+      // Required to prevent Rapier issue: https://github.com/dimforge/rapier.js/issues/278
+      treeshake: false
+    }
   },
   plugins: [wasm(), topLevelAwait()],
   resolve: {
