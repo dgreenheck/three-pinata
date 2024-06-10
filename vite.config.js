6820290fca9c646c95546d2722baa88e51c7c31c
Daniel Greenheck <dgreenheck@gmail.com>
2024-06-09 22:11:48 -0500

    Fixed issue with .d.ts not being generated

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,14 @@
 import { defineConfig } from 'vite';
+import dts from 'vite-plugin-dts';
 import path from 'path';
 
 export default defineConfig({
   build: {
+    plugins: [dts({
+      outputDir: './dist',
+      staticImport: true,
+      insertTypesEntry: true
+    })],
     lib: {
       entry: path.resolve(__dirname, 'src/index.ts'),
       name: 'three-pinata',
